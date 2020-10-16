77a0dc8acc18899c36c22ad3de260f3f75e64f32
Sandeep Gupta <sandeepg.97@gmail.com>
2020-10-16 10:29:01 +0530

    Fooling the linker (Rise of c++ mangling)

---
+++ linker/fooling/add.h
@@ -0,0 +1,6 @@
+#ifndef _ADD_H_
+	#define _ADD_H_
+
+int add(int, int, int, int);
+
+#endif // _ADD_H_
---
+++ linker/fooling/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "add.h"
+
+int main()
+{
+	int a = add(1, 2, 3, 4); // 10
+	printf("%d", a);
+
+	return !printf("\n");
+}
